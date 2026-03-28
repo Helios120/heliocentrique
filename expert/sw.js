abc8ac736d18b4888f269e4bec32eb7b90675a68
Helios120 <79373191+Helios120@users.noreply.github.com>
2026-03-28 17:21:11 +0100

    Create sw.js

---
+++ expert/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "heliosastro-expert-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./assets/expert.css",
+  "./assets/expert.js",
+  "./assets/logo-heliosastro.png",
+  "./assets/roue-heliosastro.png",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
