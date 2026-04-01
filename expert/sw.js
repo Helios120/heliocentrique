df726263e16d6ebc4ba63153563ec938c077e384
Helios120 <79373191+Helios120@users.noreply.github.com>
2026-04-01 08:50:19 +0200

    Update sw.js
    neutralize expert sw

--- expert/sw.js
+++ expert/sw.js
@@ -1,16 +1,8 @@
-self.addEventListener("install", (event) => {
-  self.skipWaiting();
-});
-
+self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => caches.delete(key)));
-    }).then(() => self.clients.claim())
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener("fetch", () => {
-  // Service worker neutralisé :
-  // aucun cache, aucune interception.
-});
+self.addEventListener("fetch", () => {});
